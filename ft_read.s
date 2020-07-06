ce5d780abe51e0b300fad64af01b4ff77cda9dbb
Nath-G <nathalie.greselin@gmail.com>
2020-07-06 20:31:49 +0200

    happy ytest

--- ft_read.s
+++ ft_read.s
@@ -1,13 +1,12 @@
-;section .bss         ;Variable segment
- ; extern  __errno_location ;
+section .bss         ;Variable segment
+   extern  __errno_location ;
    
 section .text          ;Code Segment
-   global ft_write
-
-   extern  __errno_location ;
-   ft_write:                
-      mov rax, 1		; set call to write
-      syscall					; call kernel so rax (write)
+   global ft_read
+  
+   ft_read:                
+      mov rax, 0		; set call to read
+      syscall					; call kernel so rax (read)
       mov   r8, rax
       cmp r8, 0;
       jnge exit_error		; if doesn't work, jmp exit error
@@ -18,12 +17,12 @@ section .text          ;Code Segment
       ;mov rbp, rsp
       neg r8;
       call __errno_location
-      mov qword[rax], r8  ;à terme r8 le num d'errur en faisant un neg avant?
+      mov [rax], r8  ;
       mov rax, -1				; set return to -1
       ;mov rsp, rbp
      ; pop rbp
       ret						; return
 
    exit:
-	   mov rax, rdx				; set previous value of rdx, in return value
+	   mov rax, rdx			; set previous value of rdx, in return value
       ret						; return 
