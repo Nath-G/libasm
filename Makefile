cbe5eb412d274446d55697112c82e8151d737f15
Nath-G <nathalie.greselin@gmail.com>
2020-07-08 16:50:36 +0200

    minor updates

---
+++ Makefile
@@ -0,0 +1,64 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: nagresel <nagresel@student.42.fr>          +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/10/21 16:30:26 by nagresel          #+#    #+#              #
+#    Updated: 2020/04/18 11:41:05 by user42           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+# NAME - COMPLIATOR - FLAGGS -LIBFT ****************************************** #
+NAME		= libasm.a
+OS			= $(shell uname)
+CC			= gcc
+CC_L		= clang
+NASM		= nasm
+NA_FLAGS	= f macho64
+NA_L_FLAGS	= -felf64
+CFLAGS	= -Wall -Wextra -Werror
+RM		= rm -f
+
+
+# SRCS *********************************************************************** #
+SRCS			= ft_strlen.s ft_strcpy.s ft_strcmp.s ft_write.s ft_read.s \
+					ft_strdup.s
+
+
+# OBJS *********************************************************************** #
+
+OBJS			=	${SRCS:.s=.o}
+
+OBJSBONUS		=	${SRCSBONUS:.s=.o}
+
+
+# HEADERS ******************************************************************** #
+HEADER 			= libasm.h 
+
+#HEADERBONUS		= libft_bonus.h
+
+
+# RULES ********************************************************************** #
+.s.o:
+ifeq (${OS}, Darwin)
+			${NASM} ${NA_FLAGS} -s $< -o ${<:.s=.o}
+else
+	${NASM} ${NA_L_FLAGS} -s $< -o ${<:.s=.o}
+endif
+
+$(NAME):	${OBJS}
+			ar rc ${NAME} ${OBJS} ${HEADER}
+
+all:		${NAME}
+
+clean:
+			${RM} ${OBJS} ${OBJSBONUS}
+
+fclean:		clean
+			${RM} ${NAME}
+
+re:			fclean all
+
+.PHONY:	all bonus clean fclean re
--- main.c
+++ main.c
@@ -6,23 +6,6 @@
 
 #define FT_STRCPY_BUF_SIZE (1 << 12)
 
-void		test_ft_strdup()
-{
-	printf("\x1b[32mft_strdup :\x1b[0m\n");
-	printf("	\x1b[34m[1]\x1b[0m \x1b[36m''\x1b[0m\n");
-	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", strdup(""));
-	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", ft_strdup(""));
-	printf("	\x1b[34m[2]\x1b[0m \x1b[36m'toto'\x1b[0m\n");
-	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", strdup("toto"));
-	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", ft_strdup("toto"));
-	printf("	\x1b[34m[3]\x1b[0m \x1b[36m'0123456789'\x1b[0m\n");
-	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", strdup("0123456789"));
-	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", ft_strdup("0123456789"));
-	printf("	\x1b[34m[4]\x1b[0m \x1b[36m'NULL'\x1b[0m\n");
-	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", "SEGMENTATION FAULT strdup(NULL)");
-	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", "SEGMENTATION FAULT ft_strdup(NULL)");
-}
-
 void		test_ft_strlen()
 {
 	printf("\x1b[32m\nft_strlen :\x1b[0m\n");
@@ -58,8 +41,8 @@ void		test_ft_strcpy()
 	printf("		\x1b[33m<string.h>\x1b[0m  '%s'\n", "SEGMENTATION FAULT");
 	printf("		\x1b[33m<libasm.h>\x1b[0m  '%s'\n",  "SEGMENTATION FAULT");
 	printf("	\x1b[34m[5]\x1b[0m \x1b[36m'the\\0hidden'\x1b[0m\n");
-	printf("		\x1b[33m<string.h>\x1b[0m  '%s'\n", strcpy(buf, "the\0hidden"));
-	printf("		\x1b[33m<libasm.h>\x1b[0m  '%s'\n", ft_strcpy(buf, "the\0hidden"));
+	printf("		\x1b[33m<string.h>\x1b[0m  '%s'\n", strcpy(dst, "the\0hidden"));
+	printf("		\x1b[33m<libasm.h>\x1b[0m  '%s'\n", ft_strcpy(dst, "the\0hidden"));
 	printf("	\x1b[34m[6]\x1b[0m \x1b[36m'\"Être ange c’est étrange\" de \
 Jacques PRÉVERT, recueil : \"Fatras\".\x1b[0m\n");
 	printf("		\x1b[33m<string.h>\x1b[0m  '%s'\n", strcpy(buf, "Être Ange C’est Étrange Dit l’Ange. \
@@ -76,7 +59,7 @@ Dit l’Ange en tapant du pied Étranger vous-même Dit l’Âne Et il s’envol
 
 void		test_ft_strcmp()
 {
-	char	dst[100];
+	// char	dst[100];
 
 	printf("\x1b[32m\nft_strcmp :\x1b[0m\n");
 	printf("	\x1b[34m[1]\x1b[0m \x1b[36m''\x1b[0m\n");
@@ -219,6 +202,33 @@ void		test_ft_read()
 	close(fd);
 	remove("empty_file");
 }
+
+void		test_ft_strdup()
+{
+	char *dest;
+	char *dest2;
+
+	printf("\x1b[32mft_strdup :\x1b[0m\n");
+	printf("	\x1b[34m[1]\x1b[0m \x1b[36m''\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", dest = strdup(""));
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", dest2 = ft_strdup(""));
+	free(dest);
+	free(dest2);
+	printf("	\x1b[34m[2]\x1b[0m \x1b[36m'toto'\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", dest = strdup("toto"));
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", dest2 = ft_strdup("toto"));
+	free(dest);
+	free(dest2);
+	printf("	\x1b[34m[3]\x1b[0m \x1b[36m'0123456789'\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", dest = strdup("0123456789"));
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", dest2 = ft_strdup("0123456789"));
+	free(dest);
+	free(dest2);
+	printf("	\x1b[34m[4]\x1b[0m \x1b[36m'NULL'\x1b[0m\n");
+	printf("		\x1b[33m<string.h>\x1b[0m  %s\n", "SEGMENTATION FAULT strdup(NULL)");
+	printf("		\x1b[33m<libasm.h>\x1b[0m  %s\n", "SEGMENTATION FAULT ft_strdup(NULL)");
+}
+
 
 int main (void)
 {
